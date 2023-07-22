d7e9719726fa447e1c6ff83fcdc6218030f7c1d3
netram97 <netram97@gmail.com>
2023-07-22 15:28:15 +0200

    Fixed bug in mijn_functie_2 waar door funcie combinatie niet werkte.

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,16 +1,10 @@
 def mijn_functie_1(num):
     return(num ** 2)
 
-def mijn_functie_2(num_input):
-    # Make the list of the numbers
-    numbers = num_input.split(',')
-    numbers = [int(num) for num in numbers]
-    num_list = numbers
-
-    # Calculate stuff
-    total_sum = num_list[0] + num_list[1]
-    min_value = num_list[0] - num_list[1]
-    product = num_list[0] * num_list[1]
-    quotient = int(num_list[0] / num_list[1])
-    Teruggeefwaarde = [total_sum, min_value, product, quotient]
-    return Teruggeefwaarde
+def mijn_functie_2(a,b):
+    uitvoer_lijst = []
+    uitvoer_lijst.append(a+b)
+    uitvoer_lijst.append(a-b)
+    uitvoer_lijst.append(a*b)
+    uitvoer_lijst.append(a/b)
+    return uitvoer_lijst
